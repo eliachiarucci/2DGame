fdd526ad04f9824f9232d30f6da90ab19254bea6
Elia chiarucci <elia.chiarucci@gmail.com>
2021-04-19 23:13:26 +0200

    Items and Inventory layout

---
+++ Assets/Script/Item.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum ItemType
+    {
+        Weapon,
+        Armor,
+        Potion,
+        QuestItem,
+        Generic
+    }
+
+    public ItemType itemType;
+
+    public string itemName;
+    public string description;
+    public int price;
+    public Sprite itemSprite;
+
+    [Header("Item Details")]
+
+    public int modifyHP;
+    public int modifyMana, modifyMP, modifyStrength, modifyDodge, modifyBlock, modifyAgility, modifyStamina, modifyPhysicalDef, modifyMagicDef;
+    public int weaponDamage, armorPhysicalDef, armorMagicDef;
+    public int integrity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
